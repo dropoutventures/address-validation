0202dbbb385cf228aebf2197b7d76158ca9d42f3
Jeremy Holstein <jjjrmy@users.noreply.github.com>
2020-04-29 15:08:16 -0400

    Update AddressGeocodeServiceProvider.php

--- src/AddressGeocodeServiceProvider.php
+++ src/AddressGeocodeServiceProvider.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace DropoutVentures\LaravelAddressValidation;
+namespace Dropoutventures\LaravelAddressValidation;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Validator;
